82263c6451231f83481e67d44e0886ab477d9321
Paulo Ernesto <pauloernesto2004@gmail.com>
2025-07-31 15:13:23 -0300

    Atualizar comentarios

--- src/main/java/com/pratic/Lasagna.java
+++ src/main/java/com/pratic/Lasagna.java
@@ -1,24 +1,27 @@
 package com.pratic;
 
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
+    // Metodo para definir o tempo de cozimento
     public int expectedMinutesInOven() {
         int minutes = 40;
         return minutes;
     }
-    // TODO: define the 'remainingMinutesInOven()' method
+    // Metodo para retornar quanto tempo a lasanha esta no forno
     public int remainingMinutesInOven(int minutes) {
         int minutesRemain = 10;
         return minutes - minutesRemain;
     }
 
-    // TODO: define the 'preparationTimeInMinutes()' method
+    // Neste metodo ele pede pra retornar quantos minutos para preparar cada camada
     public int preparationTimeInMinutes(int layers) {
         int prepare = 2;
         return layers * prepare;
     }
 
-    // TODO: define the 'totalTimeInMinutes()' method
+    /* Neste metodo ele quer saber qual foi o tempo total da lasanha
+       Entao resgata o parametro minutes e o parametro layers x2
+     */
+
     public int totalTimeInMinutes(int layers, int minutes) {
         return layers * 2 + minutes;
     }
