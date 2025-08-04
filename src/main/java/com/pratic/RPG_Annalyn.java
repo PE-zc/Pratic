8894a682dd505390f55ce3dc984e088ae1dde638
Paulo Ernesto <pauloernesto2004@gmail.com>
2025-08-03 23:42:37 -0300

    Exercicio que simula um rpg

--- src/main/java/com/pratic/RPG_Annalyn.java
+++ src/main/java/com/pratic/RPG_Annalyn.java
@@ -1,4 +1,23 @@
 package com.pratic;
 
-public class RPG_Annalyn {
+class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        boolean canFastAttack =! knightIsAwake;
+        return canFastAttack;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        boolean canSpy = knightIsAwake || archerIsAwake || prisonerIsAwake;
+        return canSpy;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        boolean canSignalPrisoner =! archerIsAwake && prisonerIsAwake;
+        return canSignalPrisoner;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        boolean canFreePrisoner = (petDogIsPresent && !archerIsAwake) || (prisonerIsAwake && !petDogIsPresent && !knightIsAwake && !archerIsAwake);
+        return canFreePrisoner;
+    }
 }
