8b7a89069357bf4e861c173ccd4d3261dddceedc
Paulo Ernesto <pauloernesto2004@gmail.com>
2025-08-15 10:34:11 -0300

    Correcao dos códigos

--- src/main/java/com/pratic/Annalyn_RPG.java
+++
@@ -1,4 +0,0 @@
-package com.pratic;
-
-public class Annalyn_RPG {
-}
--- src/main/java/com/pratic/LogLevels.java
+++ src/main/java/com/pratic/LogLevels.java
@@ -1,4 +1,29 @@
 package com.pratic;
 
-public class Log {
+public class LogLevels {
+
+    public static String message(String logLine) {
+        String [] splitString = logLine.split(":");
+        String message = splitString[1];
+        return message.trim();
+    }
+
+
+    public static String logLevel(String logLine) {
+        //1 split / 2 colchetes / 3 to lower case
+        String [] splitLevel = logLine.split(":");
+        String logLevel = splitLevel[0];
+        String replaceLog = logLevel.replaceAll("\\[|\\]", "");
+        return replaceLog.trim().toLowerCase();
+    }
+
+    public static String reformat(String logLine) {
+        String [] splitMessage = logLine.split(":");
+        String message1 = splitMessage[0].toLowerCase();
+        String message2 = splitMessage[1];
+        String reformat = message2.concat(message1);
+        String replace = reformat.replaceAll("\\[(.*?)\\]", " ($1)").replaceAll("\\s+", " ");
+        return replace.trim();
+    }
 }
+
